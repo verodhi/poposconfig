4ff6fd13e8ce19a412524470401639abbb6af9a8
Shaurav Adhikari <shauravadhikari@pop-os.localdomain>
2020-05-11 21:47:58 -0500

    script changes

--- InstallTools.sh
+++ InstallTools.sh
@@ -17,16 +17,12 @@ snap install --classic skype
 snap install --classic slack
 snap install --classic deja-dup
 snap install --classic atom
-
-snap install --candidate productivity-timer
-
-# Set Docker Permission
+snap install ksnip
 
 groupadd docker
 gpasswd -a $USER docker
 
 # Install Anki Web
-
 wget https://github.com/ankitects/anki/releases/download/2.1.24/anki-2.1.24-linux-amd64.tar.bz2
 tar xjf Downloads/anki-2.1.24-amd64.tar.bz2
 cd anki-2.1.24-linux-amd64
