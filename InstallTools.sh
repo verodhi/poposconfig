54922610dc7b693a7570667253228dc4a9284838
Shaurav Raj Adhikari <shauravraj@hotmail.com>
2020-06-21 18:05:38 -0500

    Flatpak it is

--- InstallTools.sh
+++ InstallTools.sh
@@ -9,10 +9,11 @@ apt -y remove firefox libreoffice-common libreoffice-core docker
 apt -y update
 apt -y autoremove
 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub
 flatpak install -y org.mozilla.firefox
 flatpak install -y com.obsproject.Studio
-flatpak install -y chromium org.remmina.Remmina
+flatpak install -y org.remmina.Remmina
 flatpak install -y org.libreoffice.LibreOffice
 flatpak install -y org.gimp.GIMP
 flatpak install -y org.blender.Blender
@@ -36,6 +37,7 @@ flatpak install -y org.gnome.Chess
 
 # Install Docker
 apt remove docker docker-engine docker.io containerd runc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
@@ -44,14 +46,14 @@ groupadd docker
 gpasswd -a $USER docker
 
 # Install Anki Web
-wget https://github.com/ankitects/anki/releases/download/2.1.26/anki-2.1.26-linux-amd64.tar.bz2
-cd $HOME/Downloads
-tar xjf anki-2.1.26-linux-amd64.tar.bz2
-cd anki-2.1.26-linux-amd64
-make install
-cd $HOME/Downloads
-rm -r anki-2.1.26-linux-amd64
-rm anki-2.1.26-linux-amd64.tar.bz2
+#wget https://github.com/ankitects/anki/releases/download/2.1.26/anki-2.1.26-linux-amd64.tar.bz2
+#cd $HOME/Downloads
+#tar xjf anki-2.1.26-linux-amd64.tar.bz2
+#cd anki-2.1.26-linux-amd64
+#make install
+#cd $HOME/Downloads
+#rm -r anki-2.1.26-linux-amd64
+#rm anki-2.1.26-linux-amd64.tar.bz2
 
 apt -y update
 apt -y upgrade
