a0bde258742addc0da0ebebcdd96bb312207c272
Shaurav Raj Adhikari <shauravraj@hotmail.com>
2021-05-30 12:42:09 -0500

    Update InstallTools.sh

--- InstallTools.sh
+++ InstallTools.sh
@@ -70,6 +70,9 @@ apt -y install docker-ce docker-ce-cli containerd.io
 # Install python packages
 
 apt -y install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+
+# Install rclone
+curl https://rclone.org/install.sh | bash
 # wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
 # bash Anaconda3-2020.02-Linux-x86_64.sh
 
